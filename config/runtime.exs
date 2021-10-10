0e89953cf5016ffe8cfd4488bf9d9e9e6743b94d
Miguel Coba <miguel.coba@gmail.com>
2021-10-10 19:05:56 +0200

    Set up Elixir Releases

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
   # If you are doing OTP releases, you need to instruct Phoenix
   # to start each relevant endpoint:
   #
-  #     config :saturn, SaturnWeb.Endpoint, server: true
+      config :saturn, SaturnWeb.Endpoint, server: true
   #
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
